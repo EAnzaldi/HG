2051a9988129587684ee9960a864dc0037fdceea
Mike <casadidiomichele2001@gmail.com>
2024-12-22 00:45:22 +0100

    Aggiunto calderoni illuminati, spostata telecamera

--- Game/OpenGLApp/OpenGLApp/Enemy.h
+++ Game/OpenGLApp/OpenGLApp/Enemy.h
@@ -5,7 +5,7 @@
 class Enemy : public MovingObject {
 public:
   
-    Enemy(glm::vec2 position, glm::vec3 size, Model model, TextureObject texture, bool repeatWidth, glm::vec2 velocity);
+    Enemy(glm::vec2 position, glm::vec3 size, Model model, TextureObject* texture, bool repeatWidth, glm::vec2 velocity);
   
     void Move(float deltaTime);
 
